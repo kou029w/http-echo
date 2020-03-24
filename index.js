2aaa2601dd134cd0dd7d336ff55685edf5577d43
Kohei Watanabe <kou029w@gmail.com>
2020-03-24 18:21:28 +0900

    feat: add /basic-auth endpoint

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const passport = require("passport");
+const { BasicStrategy } = require("passport-http");
+const { timingSafeEqual } = require("crypto");
+const { HTTP_USERNAME, HTTP_PASSWORD } = process.env;
+
+passport.use(
+  new BasicStrategy(function(username, password, done) {
+    return done(
+      null,
+      HTTP_USERNAME != null &&
+        username.length === HTTP_USERNAME.length &&
+        timingSafeEqual(Buffer.from(username), Buffer.from(HTTP_USERNAME)) &&
+        password.length === HTTP_PASSWORD.length &&
+        timingSafeEqual(Buffer.from(password), Buffer.from(HTTP_PASSWORD))
+    );
+  })
+);
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.raw({ type: "*/*" }));
 app.set("json spaces", 2);
+app.all("/basic-auth", passport.authenticate("basic", { session: false }));
 app.all("/*", function(req, res) {
   res.json({
     headers: req.headers,
