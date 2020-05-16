9e71465dc97f82f40fef28dcad88769d36a955b2
Kohei Watanabe <kou029w@gmail.com>
2020-05-16 22:07:54 +0900

    resolve file path for zip-it-and-ship-it

--- netlify-function.js
+++ netlify-function.js
@@ -1,5 +1,5 @@
 const awsServerlessExpress = require("aws-serverless-express");
-const app = require("../../index.js");
+const app = require("./index.js");
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
